7a31f8f03f46a2770099039ff8b1b4534df31cb6
Minya Bai <minyabai25@gmail.com>
2021-11-20 23:07:54 -0500

    Added code for plotting figure 3

--- CodingProject1/radiative_convective_model.py
+++ CodingProject1/radiative_convective_model.py
@@ -5,23 +5,37 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import ticker
 from scipy.optimize import fsolve
 from scipy.special import gammaincc, gamma
+from scipy.integrate import solve_ivp
 
 ## When defining function, Gamma(x,y) = gammaincc(1+x)*gamma()
 
-def model1(y,x,a): # Equation 30
+def model1(y,x,a): # Equation 30 for Figure 1
     # Define x = 4\Beta/n, y = D\tau_{0}, a = D\tau_{rc}
     gammas = gammaincc(1+x,a)*gamma(1+x) - gammaincc(1+x,y)*gamma(1+x)
     return (y/a)**x*np.exp(-(y-a))*(1+(np.exp(y)/(y**x))*(gammas))-(2+a)/(1+a)
     
-def model2(y,x): # Equation 31
+def model2(y,x): # Equation 31 for Figure 2
     return gammaincc(1+x,y)*gamma(1+x)/(y**x*np.exp(-y))-(2+y)/(1+y)
 
-def model2_sagan(y,x): # Equation 32
+def model2_sagan(y,x): # Equation 32 for Figure 2
     return y/(x*(1+y))-1
 
+def model3(t,y,kD): # Equation 33 for Figure 3
+    # Def x = T/T_o, y = P/P_o
+    # x,y = z
+    tau = tau_o*t**n
+    k = kD*D
+    # return [t**(-4)*y*(np.exp(k*tau)/(1-kD)+1)/(n*k*tau)]
+    # x,y = z
+    # tau = tau_o*y**n
+    # k = kD * D
+    return [t**(-1)*y*(n*k*tau*((1-kD**2)*np.exp(-k*tau)/(kD+1+(kD**2-1)*np.exp(-k*tau))))]
+
+def model8(): # Equation [] for Figure 8
+    return 8
+    
 n_args = 1001
 x = np.linspace(0.2,1,n_args)
 
@@ -73,3 +87,29 @@ plt.show()
 
 ## Figure 3
 ## ===============================
+# Constants
+n = 2
+tau_o = 2
+D = 5/3
+
+# Initial condition and arguments
+k_D = [0.5,0.1,10]
+t0 = [0.76,0.57,4.1]
+pp0 = np.linspace(0.1,2,n_args)
+
+fig3,ax3 = plt.subplots()
+
+for i in range(len(k_D)):
+    sol = solve_ivp(model3,t_span=[pp0[0],pp0[-1]],y0=[t0[i]],t_eval=pp0,args=[k_D[i]])
+    
+    ax3.loglog(sol.y.flatten(),sol.t,label='k/D={}'.format(k_D[i]))
+    
+ax3.set_ylim(0.1,2)
+ax3.set_xlim(0.4,10)
+ax3.tick_params(top=True,axis='x',direction='in')
+ax3.tick_params(right=True,axis='y',direction='in')
+ax3.tick_params(which='minor', right=True, axis='y', direction='in')
+ax3.tick_params(which='minor', top=True, axis='x', direction='in')
+ax3.legend()
+plt.gca().invert_yaxis()
+plt.show()
