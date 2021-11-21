255b7e56a3a9ea5834cc2f7047611d030918a5b3
Minya Bai <minyabai25@gmail.com>
2021-11-21 00:37:48 -0500

    Added code for plotting figure8

--- CodingProject1/radiative_convective_model.py
+++ CodingProject1/radiative_convective_model.py
@@ -28,12 +28,24 @@ def model3(t,y,kD): # Equation 33 for Figure 3
     k = kD*D
     return [t**(-1)*y*(n*k*tau*((1-kD**2)*np.exp(-k*tau)/(kD+1+(kD**2-1)*np.exp(-k*tau))))]
 
-def model8(): # Equation [] for Figure 8
-    return 8
+def model8(T,n,tau_0,T1): # Convective Model
+    p0 = 92
+    T0 = 730
+    gamma = 1.3
+    alpha = 0.8
+    sigma = 5.67e-8
+    D = 3/2
+    F_net = 160
+    F_i = 0
+    
+    if T < T1:
+        return p0*((2*sigma*T**4)/(D*tau_0*(F_net+F_i))-1/(D*tau_0))**(1/n)
+    else:
+        return p0*(T/T0)**(gamma/(alpha*(gamma-1)))
     
 n_args = 1001
 x = np.linspace(0.2,1,n_args)
-
+'''
 ## Figure 1
 ## ===============================
 Dtrc = [0.01,0.1,0.5,1.0,2.0]
@@ -123,6 +135,30 @@ ax3.legend()
 plt.gca().invert_yaxis()
 plt.tight_layout()
 plt.show()
-
+'''
 ## Figure 4
 ## ========================
+fig4,ax4 = plt.subplots()
+
+temps = np.linspace(170,800,n_args)
+
+# n = 1, tau_0 = 400
+p1 = [model8(i,1,400,260) for i in temps]
+ax4.plot(temps,p1,color='xkcd:navy',linestyle=':',label='n=1')
+
+# n = 2, tau_0 = 2x10^{5}
+p2 = [model8(i,2,2e5,215) for i in temps]
+ax4.plot(temps,p2,color='orangered',linestyle='--',label='n=2')
+
+ax4.set_xlabel('Temperature [K]')
+ax4.set_ylabel('Pressure [bar]')
+ax4.set_yscale('log')
+ax4.set_ylim(0.01,100)
+ax4.set_xlim(150,800)
+ax4.tick_params(top=True,axis='x',direction='in') 
+ax4.tick_params(right=True,axis='y',direction='in')
+ax4.tick_params(which='minor', right=True, axis='y', direction='in')
+ax4.tick_params(which='minor', top=True, axis='x', direction='in') 
+ax4.legend()
+plt.gca().invert_yaxis()
+plt.show()
