a72d38ade96b992236102a8f8318b696dbdb30b5
Minya Bai <minyabai25@gmail.com>
2021-11-22 15:22:41 -0500

    initial commit for troposphere and stratosphere model

---
+++ CodingProject1/trop_strat_model.py
@@ -0,0 +1,9 @@
+## PHYS404 - Coding Project 1
+## ===============================
+## Minya Bai (260856843)
+## Plotting Earth's troposphere and stratosphere
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
